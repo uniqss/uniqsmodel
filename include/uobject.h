168753459bd247a81f869174bac59d06fd288830
uniqs <uniqs@163.com>
2022-03-15 07:08:37 +0000

    code optimize

--- include/uobject.h
+++ include/uobject.h
@@ -20,24 +20,24 @@ class UObject {
     std::unordered_map<std::string, str> m_properties_str;
 
     // 多值不随机查询 arr
-    std::unordered_map<std::string, std::vector<i32>> m_properties_table_arr_i32;
-    std::unordered_map<std::string, std::vector<u32>> m_properties_table_arr_u32;
-    std::unordered_map<std::string, std::vector<i64>> m_properties_table_arr_i64;
-    std::unordered_map<std::string, std::vector<u64>> m_properties_table_arr_u64;
-    std::unordered_map<std::string, std::vector<str>> m_properties_table_arr_str;
+    std::unordered_map<std::string, std::vector<i32>> m_arr_i32;
+    std::unordered_map<std::string, std::vector<u32>> m_arr_u32;
+    std::unordered_map<std::string, std::vector<i64>> m_arr_i64;
+    std::unordered_map<std::string, std::vector<u64>> m_arr_u64;
+    std::unordered_map<std::string, std::vector<str>> m_arr_str;
     // 多值随机查询 set
-    std::unordered_map<std::string, std::unordered_set<i32>> m_properties_table_set_i32;
-    std::unordered_map<std::string, std::unordered_set<u32>> m_properties_table_set_u32;
-    std::unordered_map<std::string, std::unordered_set<i64>> m_properties_table_set_i64;
-    std::unordered_map<std::string, std::unordered_set<u64>> m_properties_table_set_u64;
-    std::unordered_map<std::string, std::unordered_set<str>> m_properties_table_set_str;
+    std::unordered_map<std::string, std::unordered_set<i32>> m_set_i32;
+    std::unordered_map<std::string, std::unordered_set<u32>> m_set_u32;
+    std::unordered_map<std::string, std::unordered_set<i64>> m_set_i64;
+    std::unordered_map<std::string, std::unordered_set<u64>> m_set_u64;
+    std::unordered_map<std::string, std::unordered_set<str>> m_set_str;
 
     // map
-    std::unordered_map<std::string, std::unordered_map<int64_t, i32>> m_properties_map_i32;
-    std::unordered_map<std::string, std::unordered_map<int64_t, u32>> m_properties_map_u32;
-    std::unordered_map<std::string, std::unordered_map<int64_t, i64>> m_properties_map_i64;
-    std::unordered_map<std::string, std::unordered_map<int64_t, u64>> m_properties_map_u64;
-    std::unordered_map<std::string, std::unordered_map<int64_t, str>> m_properties_map_str;
+    std::unordered_map<std::string, std::unordered_map<int64_t, i32>> m_map_i32;
+    std::unordered_map<std::string, std::unordered_map<int64_t, u32>> m_map_u32;
+    std::unordered_map<std::string, std::unordered_map<int64_t, i64>> m_map_i64;
+    std::unordered_map<std::string, std::unordered_map<int64_t, u64>> m_map_u64;
+    std::unordered_map<std::string, std::unordered_map<int64_t, str>> m_map_str;
 
     // sub objects
     std::unordered_map<std::string, std::unordered_set<int64_t>> m_sub_objects_set;
